be5c809a7767c728686b1514152e9eea334dcb7f
yigiterozan06-alt <yigiterozan06@gmail.com>
2026-08-05 14:18:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// İlerleyen süreçte dinamik efektler veya API çekimleri eklemek için temiz JS yapısı
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Yiğit Erozan Resmi Web Sitesi Yüklendi.");
+});

71b79f84a6d8f73d3ccd09344ca67f3c034244d5
yigiterozan06-alt <yigiterozan06@gmail.com>
2026-08-05 18:52:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-// İlerleyen süreçte dinamik efektler veya API çekimleri eklemek için temiz JS yapısı
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Yiğit Erozan Resmi Web Sitesi Yüklendi.");
-});
+console.log("Yiğit Erozan Official Website");
